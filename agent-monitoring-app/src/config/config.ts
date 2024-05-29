3dfa1d8135f8f5cab85b969b9d2e6aef4f6f88d9
xihzhang001 <xihzhang@outlook.com>
2024-05-29 12:23:30 +0800

    url

--- agent-monitoring-app/src/config/config.ts
+++ agent-monitoring-app/src/config/config.ts
@@ -1,5 +1,5 @@
 export default {
   clientId: 'b2abe3c9-d98d-46bb-96ce-ff74237ec63f',
-  // redirectUri: 'https://genesyscloudblueprints.github.io/agent-monitoring-app-blueprint/'
-  redirectUri: 'http://localhost:8080/'
+  redirectUri: 'https://xihzhang001.github.io/web-socket-agent-status/'
+  //redirectUri: 'http://localhost:8080/'
 }
